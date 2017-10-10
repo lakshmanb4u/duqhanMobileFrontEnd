0444ff6e39b510695b81e4567f3b9464e203069f
Krisanu Nandi <Krisanu Nandi>
2017-10-10 12:08:37 +0530

    updated product details

--- app/store/controllers/product-ctrl.js
+++ app/store/controllers/product-ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 angular
-  .module( 'store' )
-  .controller( 'ProductCtrl', function (
+  .module('store')
+  .controller('ProductCtrl', function (
     $log,
     $stateParams,
     $ionicSlideBoxDelegate,
@@ -12,6 +12,7 @@ angular
     $rootScope,
     $ionicPopup,
     $location,
+    $sce,
     Store,
     BusyLoader
   ) {
@@ -23,9 +24,9 @@ angular
       'Hello from your Controller: ProductCtrl in module store:. This is your controller:',
       ctrl
     );
-    $log.log( 'Current location ==============================' );
-    $log.log( $location.path() );
-    $log.log( 'Current location ==============================' );
+    $log.log('Current location ==============================');
+    $log.log($location.path());
+    $log.log('Current location ==============================');
 
     /*=============================================
     =            Get product details            =
@@ -37,38 +38,38 @@ angular
 
     /*----------  Get details of a product from backend  ----------*/
 
-    ctrl.loadProductDetail = function ( productId ) {
+    ctrl.loadProductDetail = function (productId) {
       var productParam = { productId: productId };
       BusyLoader.show();
-      Store.getProductDetail( productParam )
-        .then( function ( response ) {
-          $log.log( response.data );
+      Store.getProductDetail(productParam)
+        .then(function (response) {
+          $log.log(response.data);
           ctrl.product = response.data;
-          $log.log( ctrl.images );
-          $log.log( ctrl.product.images );
+          $log.log(ctrl.images);
+          $log.log(ctrl.product.images);
           var sizeArr = [];
-          if ( ctrl.product.sizes ) {
-            angular.forEach( ctrl.product.sizes, function ( value ) {
-              sizeArr.push( value.sizeText );
-            } );
+          if (ctrl.product.sizes) {
+            angular.forEach(ctrl.product.sizes, function (value) {
+              sizeArr.push(value.sizeText);
+            });
           }
           ctrl.product.sizeArr = sizeArr.toString();
           var colorArr = [];
-          if ( ctrl.product.sizes ) {
-            angular.forEach( ctrl.product.colors, function ( value ) {
-              colorArr.push( value.colorText );
-            } );
+          if (ctrl.product.sizes) {
+            angular.forEach(ctrl.product.colors, function (value) {
+              colorArr.push(value.colorText);
+            });
           }
           ctrl.product.colorArr = colorArr.toString();
-          $ionicSlideBoxDelegate.$getByHandle( 'image-viewer' ).update();
+          $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
           ctrl.productId = ctrl.product.productId;
-        } )
-        .catch( function ( response ) {
-          $log.log( response );
-        } )
-        .finally( function () {
+        })
+        .catch(function (response) {
+          $log.log(response);
+        })
+        .finally(function () {
           BusyLoader.hide();
-        } );
+        });
     };
 
     /*----------  Storing url parameter (product id) in scope ----------*/
@@ -77,21 +78,55 @@ angular
 
     /*----------  call the function at the time of initialization  ----------*/
 
-    ctrl.loadProductDetail( $stateParams.productId );
+    ctrl.loadProductDetail($stateParams.productId);
 
     /*=====  End of Get product details  ======*/
+
+    /*==================================================
+    Section: Property List
+    ==================================================*/
+    ctrl.trustAsHtml = function (string) {
+      return $sce.trustAsHtml(string);
+    };
+    $ionicModal
+      .fromTemplateUrl('store/templates/product/propertyListModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      })
+      .then(function (modal) {
+        ctrl.propertyListModal = modal;
+      });
+    ctrl.openPropertyList = function (p) {
+      ctrl.propertyList = p;
+      $log.log('==========================================================');
+      $log.log(ctrl.propertyList);
+      $log.log('==========================================================');
+      ctrl.propertyListModal.show();
+    };
+    ctrl.closePropertyListModal = function () {
+      ctrl.propertyListModal.hide();
+    };
+
+    ctrl.propertySelected = function (property) {
+      $log.log(property);
+      ctrl.propertyList.selectedProperty = property.value;
+      ctrl.propertyListModal.hide();
+    };
+    /*==================================================
+    End: Property List
+    ==================================================*/
 
     /*==========================================================================================
   =            Helping functions to traverse through tabs in product details page            =
   ==========================================================================================*/
 
     ctrl.gotoDescription = function () {
-      $log.log( ctrl.productId );
-      $state.go( 'store.product.description', { productId: ctrl.productId } );
+      $log.log(ctrl.productId);
+      $state.go('store.product.description', { productId: ctrl.productId });
     };
 
     ctrl.gotoRelated = function () {
-      $state.go( 'store.product.related', { productId: ctrl.productId } );
+      $state.go('store.product.related', { productId: ctrl.productId });
     };
 
     /*=====  End of Helping functions to traverse through tabs in product details page  ======*/
@@ -102,76 +137,76 @@ angular
 
     /*----------  Add to cart - get triggered when user press "Add to Bag" button from the product detail page ----------*/
 
-    ctrl.addToBag = function ( product ) {
+    ctrl.addToBag = function (product) {
       ctrl.productSelected = {};
       ctrl.productSelected.productId = product.productId;
       // $log.log(product);
-      if ( !product.sizes || !product.sizes.length > 0 ) {
+      if (!product.sizes || !product.sizes.length > 0) {
         return;
       }
-      if ( product.sizes[ 0 ].sizeId ) {
+      if (product.sizes[0].sizeId) {
         ctrl.sizeModal();
-      } else if ( product.sizes[ 0 ].sizeColorMap[ 0 ].colorId ) {
-        ctrl.productSelected.size = ctrl.product.sizes[ 0 ];
+      } else if (product.sizes[0].sizeColorMap[0].colorId) {
+        ctrl.productSelected.size = ctrl.product.sizes[0];
         ctrl.colorModal();
       } else {
         ctrl.productSelected.mapId =
-          ctrl.product.sizes[ 0 ].sizeColorMap[ 0 ].mapId;
-        ctrl.addToBagPersist( ctrl.productSelected, ctrl.product );
+          ctrl.product.sizes[0].sizeColorMap[0].mapId;
+        ctrl.addToBagPersist(ctrl.productSelected, ctrl.product);
       }
     };
 
     /*----------  Send the product to the backend which user newly added to the cart  ----------*/
 
-    ctrl.addToBagPersist = function ( productSelected, product ) {
+    ctrl.addToBagPersist = function (productSelected, product) {
       // delete productSelected.size;
       // $log.log(ctrl.productSelected);
 
-      Store.addToCart( productSelected )
-        .then( function ( response ) {
-          $log.log( response.data );
-          if ( response.data.status === 'success' ) {
+      Store.addToCart(productSelected)
+        .then(function (response) {
+          $log.log(response.data);
+          if (response.data.status === 'success') {
             productSelected.response = 'Item Added to your Bag!';
-            ctrl.openModal( productSelected, product );
-            $rootScope.$emit( 'getCartTotalNumber' );
-          } else if ( response.data.status === 'Product already added' ) {
+            ctrl.openModal(productSelected, product);
+            $rootScope.$emit('getCartTotalNumber');
+          } else if (response.data.status === 'Product already added') {
             productSelected.response = 'Item is already in the Bag!';
-            ctrl.openModal( productSelected, product );
+            ctrl.openModal(productSelected, product);
           } else {
             ctrl.showAlert = function () {
-              var alertPopup = $ionicPopup.alert( {
+              var alertPopup = $ionicPopup.alert({
                 title: 'Out of Stock',
                 template: 'Oops! You just missed the last item in stock. It got sold out. Hurry up and purchase the items you like before they get sold out too.'
-              } );
+              });
 
-              alertPopup.then( function () {
-                $state.go( 'store.products.latest' );
-              } );
+              alertPopup.then(function () {
+                $state.go('store.products.latest');
+              });
             };
             ctrl.showAlert();
           }
-        } )
-        .catch( function ( response ) {
-          $log.log( response );
-        } );
+        })
+        .catch(function (response) {
+          $log.log(response);
+        });
     };
 
     /*----------  Ionic modal to show the response of addition of the product to the cart  ----------*/
 
     $ionicModal
-      .fromTemplateUrl( 'store/templates/product/addedToBagModal.html', {
+      .fromTemplateUrl('store/templates/product/addedToBagModal.html', {
         scope: $scope,
         animation: 'slide-in-up'
-      } )
-      .then( function ( modal ) {
+      })
+      .then(function (modal) {
         ctrl.modal = modal;
-      } );
+      });
 
-    ctrl.openModal = function ( productSelected, product ) {
+    ctrl.openModal = function (productSelected, product) {
       ctrl.productSelected = productSelected;
       ctrl.product = product;
-      $log.log( 'productSelected======' );
-      $log.log( ctrl.productSelected );
+      $log.log('productSelected======');
+      $log.log(ctrl.productSelected);
       ctrl.modal.show();
     };
 
@@ -183,60 +218,60 @@ angular
 
     ctrl.sizeModal = function () {
       var buttons = [];
-      angular.forEach( ctrl.product.sizes, function ( value ) {
-        buttons.push( { text: value.sizeText } );
-      } );
-      $ionicActionSheet.show( {
+      angular.forEach(ctrl.product.sizes, function (value) {
+        buttons.push({ text: value.sizeText });
+      });
+      $ionicActionSheet.show({
         buttons: buttons,
         titleText: 'Select size',
         cancelText: 'Cancel',
         cancel: function () {
           // add cancel code..
         },
-        buttonClicked: function ( index ) {
+        buttonClicked: function (index) {
           // $log.log(index);
           // $log.log(ctrl.product.sizes[index]);
-          ctrl.productSelected.size = ctrl.product.sizes[ index ];
-          if ( ctrl.productSelected.size.sizeColorMap ) {
+          ctrl.productSelected.size = ctrl.product.sizes[index];
+          if (ctrl.productSelected.size.sizeColorMap) {
             ctrl.colorModal();
           }
           return true;
         }
-      } );
+      });
     };
 
     /*----------  Open a Action sheet to select the color of the product  ----------*/
 
     ctrl.colorModal = function () {
       var buttons = [];
-      angular.forEach( ctrl.productSelected.size.sizeColorMap, function ( value ) {
-        buttons.push( { text: value.colorText } );
-      } );
+      angular.forEach(ctrl.productSelected.size.sizeColorMap, function (value) {
+        buttons.push({ text: value.colorText });
+      });
 
-      $ionicActionSheet.show( {
+      $ionicActionSheet.show({
         buttons: buttons,
         titleText: 'Select color',
         cancelText: 'Cancel',
         cancel: function () {
           // add cancel code..
         },
-        buttonClicked: function ( index ) {
+        buttonClicked: function (index) {
           // $log.log(index);
           // $log.log(ctrl.productSelected.size.sizeColorMap[index]);
           ctrl.productSelected.size.sizeColor =
-            ctrl.productSelected.size.sizeColorMap[ index ];
+            ctrl.productSelected.size.sizeColorMap[index];
           ctrl.productSelected.mapId =
             ctrl.productSelected.size.sizeColor.mapId;
           // $log.log(ctrl.productSelected.mapId);
-          ctrl.addToBagPersist( ctrl.productSelected, ctrl.product );
+          ctrl.addToBagPersist(ctrl.productSelected, ctrl.product);
           return true;
         }
-      } );
+      });
     };
 
     /*=====  End of Add a product to the cart  ======*/
 
-    $scope.$on( '$ionicView.beforeEnter', function ( event, viewData ) {
+    $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
       viewData.enableBack = true;
-    } );
-  } );
+    });
+  });
